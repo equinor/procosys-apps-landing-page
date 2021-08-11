851f2ce20f915f6aa4441d4471ad1b5a0e2e14b5
Erlend T Lygre <erlend.tl2@gmail.com>
2021-08-11 14:38:07 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <a href={'https://apps.procosys.com/mc'}>
+<button>Go to MC app</button>
+      </a>
+      <a href={'https://apps.procosys.com/comm'}>
+        <button>Go to Comm app</button>
+      </a>
     </div>
   );
 }
