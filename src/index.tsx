8c20da2471ee780ff08f8521f63804de3dd3597a
Victor Prestholm <vipr@equinor.com>
2022-04-08 15:22:57 +0200

    Fixed linting

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
 import GlobalStyles from './GlobalStyles';
 
 const container = document.getElementById('root');
-
 const root = ReactDOM.createRoot(container as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <GlobalStyles />
     <App />
-  </React.StrictMode>,
- 
+  </React.StrictMode>
 );
