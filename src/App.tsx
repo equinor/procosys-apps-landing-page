b6ca17947518bc7c286f39499d1a96d8128b27bd
sondreravndal <sondre.pedersen@bouvet.no>
2024-06-14 07:33:26 +0200

    chore: change redirect path

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Button, Typography } from '@equinor/eds-core-react';
-import styled from 'styled-components';
-import mcLogo from './mclogo.png';
-import commLogo from './commlogo2.png';
+import React from "react";
+import { Button, Typography } from "@equinor/eds-core-react";
+import styled from "styled-components";
+import mcLogo from "./mclogo.png";
+import commLogo from "./commlogo2.png";
 
 const LandingPageWrapper = styled.main`
   width: 100vw;
@@ -10,7 +10,7 @@ const LandingPageWrapper = styled.main`
     margin-top: 0;
     margin-bottom: 12px;
   }
-`
+`;
 const AppSection = styled.section`
   height: 50vh;
   width: 100%;
@@ -18,31 +18,31 @@ const AppSection = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
 const CommAppSection = styled(AppSection)`
-  background-color: #FFECF0;
-`
+  background-color: #ffecf0;
+`;
 
 const LogoImage = styled.img`
   width: 60px;
   object-fit: contain;
-`
+`;
 
 function App() {
   return (
     <LandingPageWrapper>
       <AppSection>
-        <LogoImage src={mcLogo}/> 
-        <Typography variant={'h4'}>Mechanical Completion</Typography>
-        <a href={'https://frontend-procosys-mc-webapp-prod.radix.equinor.com'}>
-          <Button color={'danger'}>Go to app</Button>
+        <LogoImage src={mcLogo} />
+        <Typography variant={"h4"}>Mechanical Completion</Typography>
+        <a href={"https://apps.procosys.equinor.com/mc"}>
+          <Button color={"danger"}>Go to app</Button>
         </a>
       </AppSection>
       <CommAppSection>
-        <LogoImage src={commLogo}/> 
-        <Typography variant={'h4'}>Commissioning</Typography>
-        <a href={'https://frontend-procosys-commissioning-webapp-prod.radix.equinor.com'}>
-          <Button color={'danger'}>Go to app</Button>
+        <LogoImage src={commLogo} />
+        <Typography variant={"h4"}>Commissioning</Typography>
+        <a href={"https://apps.procosys.equinor.com/comm"}>
+          <Button color={"danger"}>Go to app</Button>
         </a>
       </CommAppSection>
     </LandingPageWrapper>
